b6001a98a7fc56adfde4a49c522846028a924fb6
Vasukesh Yarlagadda <vyarlaga@students.kennesaw.edu>
2018-04-20 12:59:29 -0400

    Finished Project 6

--- Homework/Homework 6/detection_funct.py
+++ Homework/Homework 6/detection_funct.py
@@ -1,45 +1,66 @@
 import numpy as np
 import cv2
+
+# func key is 1
+'''
+Description: Checks label by taking the restored image and the mask itself. It checks the states slipped, missing, white, correct
+Image is BGR.
+'''
+
+def check_label(img, res, mask):
+    if(res[191, 176, 2] == 0 and np.sum(res[270, 140:180, 2]) != 0):
+        return -1  # has slipped
+    elif(img[189, 176, 2] < 50):
+        return 0  # is missing
+        # if the image value is low 
+    elif(np.sum(mask[191, 140:170]) == 0):
+        return 1  # is white
+    else:
+        return 2  # is correct
+
+# func key is 2
+'''
+Description: Used saturation because the image has high saturation where the coke is because its tasty.
+Took the saturation channel and then thresholded it so it can be over 185 
+'''
+
+def check_fill(hsv):
+    test = cv2.threshold(hsv[:, :, 1], 185, 255, cv2.THRESH_BINARY)[1]
+
+    if(np.sum(test[120, 140:170]) != 0):
+        return -1  # 'overfilled'
+    elif(np.sum(test[141, 140:190]) != 0):
+        return 0  # 'correctly filled'
+    else:
+        return 1  # 'underfilled'
+
+# func key is 3
+
+'''
+Description: took a line at the center of the image ( a vertical line) and summed all the values which  is compared for 0 (False) or 1 (True)...swag
+'''
+def check_missing(res):
+    if(np.sum(res[:, 176, 2]) == 0):
+        return False
+    else:
+        return True
+
+'''
+Description: Took the mask and used a middle point of the cap and if point exists then return true else we return false ... money
+'''
 # func key is 4
 def check_hat(mask):
-    test = bool(mask[30,176]/255)
+    test = bool(mask[30, 176]/255)
     if test:
         return 1
     else:
         return -1
 
-# func key is 1
-def check_label(img, res, mask):
-    if(res[191,176,2]==0 and np.sum(res[270,140:180,2])!=0):
-        return -1 #has slipped
-    elif(img[189,176,2] < 50):
-        return  0 #is missing
-    elif(np.sum(mask[191,140:170]) == 0):
-        return  1#is white
-    else:
-        return  2 #is correct
-
-# func key is 3
-def check_missing(res):
-    if(np.sum(res[:,176, 2])):
-        return False
-    else:
-        return True
-
-# func key is 2
-def check_fill(hsv):
-    test = cv2.threshold(hsv[:,:,1], 185, 255, cv2.THRESH_BINARY)[1]
-
-    if(np.sum(test[120,140:170]) != 0):
-        return  -1 #'overfilled'
-    elif(np.sum(test[141,140:190]) != 0):
-        return  0 #'correctly filled'
-    else:
-        return  1 #'underfilled'
-
-
+'''
+Take integer variables and the function key which is defined by the set of equations  in which the result is presented
+'''
 def process_result(result, funcKey):
-    if(funcKey == 1): # label
+    if(funcKey == 1):  # label
         if(result == -1):
             print("label has slipped")
         elif(result == 0):
@@ -48,14 +69,14 @@ def process_result(result, funcKey):
             print("label is white")
         elif(result == 2):
             print("label is correct")
-    elif(funcKey == 2): # fill
+    elif(funcKey == 2):  # fill
         if(result == -1):
             print("bottle is overfilled")
         elif(result == 0):
             print("bottle correctly filled")
         elif(result == 1):
             print("bottle is underfilled")
-    elif(funcKey == 3): # check missing
+    elif(funcKey == 3):  # check missing
         if(result):
             print(" Bottle is not there")
         else:
--- Homework/Homework 6/main.py
+++ Homework/Homework 6/main.py
@@ -4,54 +4,48 @@ import matplotlib.pyplot as plt
 from detection_funct import *
 from os import fsencode, listdir, fsdecode
 from os.path import isfile, join
-from time import time
+from time import time, sleep
 
-data_path = 'C:/Users/Timothy/Digital-Image-Classwork/Homework/Homework 6/cocacolaPic/'
+data_path = 'C:/Users/vyarlaga/Desktop/Digital-Image-Classwork/Homework/Homework 6/cocacolaPic/'
 
 
-lower_red = np.array([0,100,100])
+# Thresholding based off color , used for detecting the color red
+lower_red = np.array([0, 100, 100])
 upper_red = np.array([10, 255, 255])
-test_lower = np.array([0,100,100])
-test_high  = np.array([10,255,255])
 
 
-'''files = [f for f in listdir(data_path) if isfile(join(data_path,f))]
-cap  = 55
-folder = fsencode(data_path)
-img  = cv2.imread(data_path + 'image001.jpg', 1)
-'''
 # Set Timer to indicate time take per image
 t0 = time()
 
-directory = fsencode(data_path)
-
+plt.ion()
 for file in listdir(data_path):
     filename = fsdecode(file)
     print(filename)
     if filename.endswith(".jpg"):
-        img = cv2.imread(join(data_path,filename),1)
+        img = cv2.imread(join(data_path, filename), 1)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower_red, upper_red)
-        res = cv2.bitwise_and(img,img, mask= mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
         hat = check_hat(mask)
-        process_result(hat,4) # get result from hat function
+        process_result(hat, 4)  # get result from hat function
         label = check_label(img, res, mask)
-        process_result(label,1) # get result from label function
+        process_result(label, 1)  # get result from label function
         fill = check_fill(hsv)
-        process_result(fill, 2) # get result from fill function
-        dt = time() - t0
+        process_result(fill, 2)  # get result from fill function
+        cv2.imshow('bottle',img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
     else:
         continue
 
-dt = t0 - time()
+    dt = time() - t0
 print('time taken ', dt)
 
-img = cv2.imread(data_path + 'image078.jpg',1)
+img = cv2.imread(data_path + 'image078.jpg', 1)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-print(np.amax(hsv[141,140:190]))
-mask = cv2.inRange(hsv, test_lower, test_high)
-res = cv2.bitwise_and(img,img, mask= mask)
+print(np.amax(hsv[141, 140:190]))
+mask = cv2.inRange(hsv, lower_red, upper_red)
+res = cv2.bitwise_and(img, img, mask=mask)
 
-plt.imshow(hsv, cmap='gray')
-
-plt.show()
+cv2.imshow('bush did 911',hsv)
