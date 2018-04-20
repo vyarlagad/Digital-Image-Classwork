1899fc33a81ad8993879410471fb90dd80437213
Timbama <timbama@gmail.com>
2018-04-20 11:52:23 -0400

    added stuff

--- Homework/Homework 6/detection_funct.py
+++ Homework/Homework 6/detection_funct.py
@@ -1,29 +1,69 @@
-def check_hat(img):
+import numpy as np
+import cv2
+# func key is 4
+def check_hat(mask):
     test = bool(mask[30,176]/255)
     if test:
-        return 'has'
+        return 1
     else:
-        return 'Does not have'
+        return -1
+
+# func key is 1
 def check_label(img, res, mask):
     if(res[191,176,2]==0 and np.sum(res[270,140:180,2])!=0):
-        return 'has slipped'
+        return -1 #has slipped
     elif(img[189,176,2] < 50):
-        return 'is missing'
+        return  0 #is missing
     elif(np.sum(mask[191,140:170]) == 0):
-        return 'is white'
+        return  1#is white
     else:
-        return 'is correct'
+        return  2 #is correct
+
+# func key is 3
 def check_missing(res):
     if(np.sum(res[:,176, 2])):
         return False
     else:
         return True
+
+# func key is 2
 def check_fill(hsv):
     test = cv2.threshold(hsv[:,:,1], 185, 255, cv2.THRESH_BINARY)[1]
 
     if(np.sum(test[120,140:170]) != 0):
-        return 'overfilled'
+        return  -1 #'overfilled'
     elif(np.sum(test[141,140:190]) != 0):
-        return 'correctly filled'
+        return  0 #'correctly filled'
     else:
-        return 'underfilled'
+        return  1 #'underfilled'
+
+
+def process_result(result, funcKey):
+    if(funcKey == 1): # label
+        if(result == -1):
+            print("label has slipped")
+        elif(result == 0):
+            print("label is missing")
+        elif(result == 1):
+            print("label is white")
+        elif(result == 2):
+            print("label is correct")
+    elif(funcKey == 2): # fill
+        if(result == -1):
+            print("bottle is overfilled")
+        elif(result == 0):
+            print("bottle correctly filled")
+        elif(result == 1):
+            print("bottle is underfilled")
+    elif(funcKey == 3): # check missing
+        if(result):
+            print(" Bottle is not there")
+        else:
+            print("bottle is there")
+    elif(funcKey == 4):
+        if(result == -1):
+            print("bottle does not have hat")
+        elif(result == 1):
+            print("bottle has hat")
+    else:
+        print("Function Not Defined")
--- Homework/Homework 6/main.py
+++ Homework/Homework 6/main.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from detection_funct import *
-from os import fsencode, listdir
+from os import fsencode, listdir, fsdecode
 from os.path import isfile, join
 from time import time
 
-path = '/cocacolaPic'
+data_path = 'C:/Users/Timothy/Digital-Image-Classwork/Homework/Homework 6/cocacolaPic/'
 
 
 lower_red = np.array([0,100,100])
@@ -23,23 +23,27 @@ img  = cv2.imread(data_path + 'image001.jpg', 1)
 # Set Timer to indicate time take per image
 t0 = time()
 
-directory = os.fsencode(path)
+directory = fsencode(data_path)
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
+for file in listdir(data_path):
+    filename = fsdecode(file)
+    print(filename)
     if filename.endswith(".jpg"):
-        img = cv2.imread(join(data_path,onlyfiles[n]),1)
+        img = cv2.imread(join(data_path,filename),1)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, lower_red, upper_red)
         res = cv2.bitwise_and(img,img, mask= mask)
         hat = check_hat(mask)
-        label = check_label(mask)
+        process_result(hat,4) # get result from hat function
+        label = check_label(img, res, mask)
+        process_result(label,1) # get result from label function
         fill = check_fill(hsv)
+        process_result(fill, 2) # get result from fill function
         dt = time() - t0
     else:
         continue
 
-
+dt = t0 - time()
 print('time taken ', dt)
 
 img = cv2.imread(data_path + 'image078.jpg',1)
